279a9d59cd7cb20d9da97d198164504a9a78ac26
Samuel Dionne-Riel <samuel@dionne-riel.com>
2025-05-24 18:10:06 -0400

    mruby-lvgui-native: Drop now outdated cross workaround
    Turns out I made a typo and wrote nativeBuildsInputs for the inputs,
    which means that with the latest Nixpkgs update, it breaks since
    `pkg-config` is needed, but was lost because of the typo.

--- boot/script-loader/mruby-lvgui-native/lvgui.nix
+++ boot/script-loader/mruby-lvgui-native/lvgui.nix
@@ -6,6 +6,7 @@
 , freetype
 , SDL2
 , libdrm
+, libevdev
 , withSimulator ? false
 }:
 
@@ -46,14 +47,6 @@ let
     ];
   });
 
-  # Allow libevdev to cross-compile.
-  libevdev = (pkgs.libevdev.override({
-    python3 = null;
-  })).overrideAttrs({nativeBuildsInputs ? [], ...}: {
-    nativeBuildInputs = nativeBuildsInputs ++ [
-      pkgs.buildPackages.python3
-    ];
-  });
   libxkbcommon = pkgs.callPackage (
     { stdenv
     , libxkbcommon
